b8a9a75e3b94ec1841f5ff1660a2d7dfc2f10504
Dan Bruce <dbruce14@gmail.com>
2018-09-22 18:04:55 -0400

    fixing empty local.gen

--- init.sh
+++ init.sh
@@ -11,15 +11,16 @@ set -e
 
 if [[ `whoami` == 'root' ]]; then
 	# create new user
-	read -p "New user: " $user
+	read -p "New user: " user
 	home="/home/$user"
 	adduser --home $home --shell /bin/bash $user
 	usermod -aG sudo $user
 
 	# raspi-config
 	hostname pi
-	sed -i -e 's/en_GB/#en_GB/g' /etc/local.gen
-	sed -i -e 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/local.gen
+	# sed -i -e 's/en_GB/#en_GB/g' /etc/local.gen
+	# sed -i -e 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/local.gen
+	echo 'en_US.UTF-8' >> /etc/local.gen
 	locale-gen en_US.UTF-8
 	sed -i -e 's/en_GB/en_US/g' /etc/default/locale
 	echo 'LC_ALL=en.US.UTF-8' >> /etc/default/locale
